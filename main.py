466c17b606d843130194acf7503530213ed27b0f
RTAkland <79452994+MarkusJoe@users.noreply.github.com>
2022-08-04 03:47:23 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -- coding:utf-8 --
+# @Author: markushammered@gmail.com
+# @Development Tool: PyCharm
+# @Create Time: 2022/8/4
+# @File Name: main.py
+
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+
+app = FastAPI()
+
+
+@app.get('/')
+@app.post('/')
+async def index():
+    """
+    Index page
+    :return:
+    """
+    return RedirectResponse(url="https://s4.s100.vip:8123", status_code=302)
+
